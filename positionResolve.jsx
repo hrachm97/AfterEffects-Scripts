56d09c1cbc480e1ea0ed567b776a87784b56b95e
hrachm97 <hrachmkrtumyan@gmail.com>
2023-02-11 01:30:18 +0400

    mvp

---
+++ positionResolve.jsx
@@ -0,0 +1,15 @@
+var std = app.project.activeItem;
+
+var layer = std.selectedLayers[0];
+var p = layer.transform.position;
+var a = layer.transform.anchorPoint;
+
+for(i = 1; i <= p.numKeys; i++) {
+    p.setValueAtKey(i, 
+        [
+            p.valueAtTime(p.keyTime(i), false)[0] - a.value[0] + layer.width/2,
+            p.valueAtTime(p.keyTime(i), false)[1] - a.value[1] + layer.height/2
+        ]
+    )
+}
+a.setValue([layer.width/2,layer.height/2])
