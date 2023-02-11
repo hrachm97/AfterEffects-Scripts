31c1b93413a02b5a980da4748c9e75e7292683b6
hrachm97 <hrachmkrtumyan@gmail.com>
2023-02-12 00:07:11 +0400

    didn't find the way to overwrite the content of shape branch into main so copied the content of shape and pasted into main

--- positionResolve.jsx
+++ positionResolve.jsx
@@ -5,15 +5,19 @@ app.beginUndoGroup("resolve position");
 var layer = std.selectedLayers[0];
 var p = layer.transform.position;
 var a = layer.transform.anchorPoint;
+var rect = layer.sourceRectAtTime(std.time, false);
+
+var x = rect.left + rect.width/2;
+var y = rect.top + rect.height/2;
 
 for(i = 1; i <= p.numKeys; i++) {
     p.setValueAtKey(i, 
         [
-            p.valueAtTime(p.keyTime(i), false)[0] - a.value[0] + layer.width/2,
-            p.valueAtTime(p.keyTime(i), false)[1] - a.value[1] + layer.height/2
+            p.valueAtTime(p.keyTime(i), false)[0] + x - a.value[0],
+            p.valueAtTime(p.keyTime(i), false)[1] + y - a.value[1]
         ]
     )
 }
-a.setValue([layer.width/2,layer.height/2])
+a.setValue([x, y]);
 
 app.endUndoGroup();
