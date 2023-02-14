c6359feb5c5bdc4e855cced5366b59f2a5f21590
hrachm97 <hrachmkrtumyan@gmail.com>
2023-02-15 00:07:08 +0400

    sadfasdf

--- inPointPasting.jsx
+++ inPointPasting.jsx
@@ -8,11 +8,14 @@ app.beginUndoGroup("inPoint pasting");
 for(i = 0; i < std.selectedLayers.length; i++) 
 {
     layers.push(std.selectedLayers[i]);
+    std.selectedLayers[i].selected = false;
 }
 
 for(i = 0; i < layers.length; i++) {
     std.time = layers[0].inPoint;
+    layers[i].selected = true;
     app.executeCommand(20);
+    layers[i].selected = false;
 }
 
 std.time = originalTime;
