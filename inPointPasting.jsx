d867596a3258f3bd14e4ce212f28e86dd3c7513b
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-02-20 23:58:02 +0400

    packaged keyframe pasting to a function

--- inPointPasting.jsx
+++ inPointPasting.jsx
@@ -1,30 +1,33 @@
 var std = app.project.activeItem;
 
-var layers = [];
+function inPointExpression() {
+    var layers = [];
 
-app.beginUndoGroup("inPoint pasting");
+    app.beginUndoGroup("inPoint pasting");
 
-for(i = 0; i < std.selectedLayers.length; i++) 
-{
-    layers.push(std.selectedLayers[i]);
+    for(i = 0; i < std.selectedLayers.length; i++) 
+    {
+        layers.push(std.selectedLayers[i]);
+    }
+
+    var tmp = std.layers.addNull();
+    app.executeCommand(20);
+    var theLayer = std.selectedLayers[0].transform.position.expression;
+
+    function getPosition(string, subString, index) {
+        return string.split(subString, index).join(subString).length;
+    }
+
+    theLayer = theLayer.substr(0, theLayer.indexOf(").t") + 1);
+
+    tmp.remove();
+
+
+
+    for(i = 0; i < layers.length; i++) {
+        layers[i].transform.position.expression = "var otherLayer = " + theLayer + ";\nvar difference = thisLayer.inPoint - otherLayer.inPoint;\notherLayer.transform.position.valueAtTime(time - difference)";
+    }
+
+    app.endUndoGroup();
 }
-
-var tmp = std.layers.addNull();
-app.executeCommand(20);
-var theLayer = std.selectedLayers[0].transform.position.expression;
-
-function getPosition(string, subString, index) {
-    return string.split(subString, index).join(subString).length;
-}
-
-theLayer = theLayer.substr(0, theLayer.indexOf(").t") + 1);
-
-tmp.remove();
-
-
-
-for(i = 0; i < layers.length; i++) {
-    layers[i].transform.position.expression = "var otherLayer = " + theLayer + ";\nvar difference = thisLayer.inPoint - otherLayer.inPoint;\notherLayer.transform.position.valueAtTime(time - difference)";
-}
-
-app.endUndoGroup();
+inPointExpression();
