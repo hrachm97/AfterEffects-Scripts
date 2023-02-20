685b17d03405ea7584aa9aea290c24c3a2307301
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-02-20 23:59:35 +0400

    Update inPointPasting.jsx

--- inPointPasting.jsx
+++ inPointPasting.jsx
@@ -30,4 +30,27 @@ function inPointExpression() {
 
     app.endUndoGroup();
 }
+
+function pasteKeys() {
+    var time = std.time;
+    var startTimes = [];
+
+    app.beginUndoGroup("inPoint pasting");
+
+    for(i = 0; i < std.selectedLayers.length; i++) {
+        startTimes.push(std.selectedLayers[i].startTime);
+        std.selectedLayers[i].startTime -= std.selectedLayers[i].inPoint;
+    }
+    std.time = 0;
+
+    app.executeCommand(20);
+
+    std.time = time;
+    for(i = 0; i < std.selectedLayers.length; i++) {
+        std.selectedLayers[i].startTime = startTimes[i];
+    }
+    
+    app.endUndoGroup();
+}
+
 inPointExpression();
