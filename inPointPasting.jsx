a82d17ae23926d9cd18bb18cfa3226834d785330
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-02-20 23:55:23 +0400

    packaged keyframe pasting to a function, to eliminate branch

--- inPointPasting.jsx
+++ inPointPasting.jsx
@@ -1,21 +1,25 @@
 var std = app.project.activeItem;
 
-var time = std.time;
-var startTimes = [];
+function pasteKeys() {
+    var time = std.time;
+    var startTimes = [];
 
-app.beginUndoGroup("inPoint pasting");
+    app.beginUndoGroup("inPoint pasting");
 
-for(i = 0; i < std.selectedLayers.length; i++) {
-    startTimes.push(std.selectedLayers[i].startTime);
-    std.selectedLayers[i].startTime -= std.selectedLayers[i].inPoint;
+    for(i = 0; i < std.selectedLayers.length; i++) {
+        startTimes.push(std.selectedLayers[i].startTime);
+        std.selectedLayers[i].startTime -= std.selectedLayers[i].inPoint;
+    }
+    std.time = 0;
+
+    app.executeCommand(20);
+
+    std.time = time;
+    for(i = 0; i < std.selectedLayers.length; i++) {
+        std.selectedLayers[i].startTime = startTimes[i];
+    }
+    
+    app.endUndoGroup();
 }
-std.time = 0;
 
-app.executeCommand(20);
-
-std.time = time;
-for(i = 0; i < std.selectedLayers.length; i++) {
-    std.selectedLayers[i].startTime = startTimes[i];
-}
-
-app.endUndoGroup();
+pasteKeys();
