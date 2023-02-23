46b5962c4a53c6889a72c18e0145e485be0f55f4
hrachm97 <hrachmkrtumyan@gmail.com>
2023-02-23 11:25:06 +0400

    recursively adjustes all child compItems frame rates. Skips those who match.

--- fpsResolve.jsx
+++ fpsResolve.jsx
@@ -1,48 +1,24 @@
 var std = app.project.activeItem;
 
-function validateSelect(count){
-   if (std.selectedLayers.length == count) return true;
-   else {
-      alert("Please select two layers!");
-      return false;
-   }
-}
+var count = 0;
 
-function deepCopy(layer, std) {
-   if (!validateSelect(1)) return;
-
-   app.beginUndoGroup("Deep Copy");
-
-   var duplicatedLayer = layer.duplicate();
-   makeSourceUnique(duplicatedLayer, std);
-
-   app.endUndoGroup();
-}
-
-function makeSourceUnique(layer, thisComp) {
-   var source;
-   if(layer.source == null) {
-      
-   } else if(layer.source instanceof CompItem){
-      source = layer.source.duplicate(); 
-      layer.replaceSource(source, true);
-      for(var i = 1; i <= layer.source.numLayers; i++) {
-         makeSourceUnique(layer.source.layer(i), layer.source);
+function setFrameRate(layer) {
+   if(layer.source instanceof CompItem){
+      if(layer.source.frameRate != std.frameRate) {
+         layer.source.frameRate = std.frameRate;
+         count++;
       }
-   } 
-   else if(layer.source instanceof FootageItem && !(layer.source.mainSource instanceof SolidSource)) {
-      var projFile = layer.source;
-      var imp = new ImportOptions();
-      imp.file = projFile.file;
-      source = app.project.importFile(imp);
-      layer.replaceSource(source, true);
-   } else if(layer.source.mainSource instanceof SolidSource) {
-      var src = layer.source;
-      var newSolid = thisComp.layers.addSolid(src.mainSource.color, src.name, src.width, src.height, src.pixelAspect, src.duration);
-      layer.replaceSource(newSolid.source, true);
-      newSolid.remove();
+      for(var i = 1; i <= layer.source.numLayers; i++) {
+         setFrameRate(layer.source.layer(i));
+      }
    }
-
 }
 
-deepCopy(std.selectedLayers[0], std);
+app.beginUndoGroup("adjusting frame rates");
+
+for(i = 0; i < std.selectedLayers.length; i++) {
+   setFrameRate(std.selectedLayers[i]);
+}
+alert(count + " comp fps have been adjusted");
+
+app.endUndoGroup();
