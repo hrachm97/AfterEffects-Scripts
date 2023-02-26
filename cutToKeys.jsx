ff0bb35b5b61246094eb7b72098d92748c1c8e91
hrachm97 <hrachmkrtumyan@gmail.com>
2023-02-26 17:33:32 +0400

    seperates layers to animated sections, but doesn't delete keyframes

--- cutToKeys.jsx
+++ cutToKeys.jsx
@@ -1,54 +1,82 @@
 var std = app.project.activeItem;
 
+function debug(msg) {
+   alert(msg);
+}
+
 function getAnimatedSections(prop) {
    var animatedTimes = [];
    var inPerod = false;
-   var prevValue = prop.valueAtTime(prop.keyTime(1), true);
+   var prevValue = prop.valueAtTime(prop.keyTime(1).toFixed(3), true);
+   //alert(prop.name);
    for(var k = 2; k <= prop.numKeys; k++) {
       if(inPerod) {
-         if((prop.valueAtTime(prop.keyTime(k), true)[0] === prevValue[0] && prop.valueAtTime(prop.keyTime(k), false)[1] === prevValue[1])) {
-            animatedTimes.push(prop.keyTime(k - 1));
+         if(JSON.stringify(prop.valueAtTime(prop.keyTime(k).toFixed(3), true)) === JSON.stringify(prevValue)) {
+            animatedTimes.push([prop.keyTime(k - 1).toFixed(3), -1]);
             inPerod = false;
          } else {
-            prevValue = prop.valueAtTime(prop.keyTime(k), true);
-            if(k === prop.numKeys) animatedTimes.push(prop.keyTime(k));
+            prevValue = prop.valueAtTime(prop.keyTime(k).toFixed(3), true);
+            if(k === prop.numKeys) animatedTimes.push([prop.keyTime(k).toFixed(3), -1]);
          }
       } else {
-         // alert(prevValue);
-         // alert(prop.valueAtTime(prop.keyTime(k), true));
-         if(prop.valueAtTime(prop.keyTime(k), true)[0] !== prevValue[0] || prop.valueAtTime(prop.keyTime(k), false)[1] !== prevValue[1]) {
-            //alert(k + "nth keyframe is equal to prevValue: " + prevValue[0] + ',' + prevValue[1] + " so pushing it's time to the array");
-            animatedTimes.push(prop.keyTime(k - 1));
-            if(k === prop.numKeys) animatedTimes.push(prop.keyTime(k));
-            prevValue = prop.valueAtTime(prop.keyTime(k), true);
+         if(JSON.stringify(prop.valueAtTime(prop.keyTime(k).toFixed(3), true)) !== JSON.stringify(prevValue)) {
+            animatedTimes.push([prop.keyTime(k - 1).toFixed(3), 1]);
+            prevValue = prop.valueAtTime(prop.keyTime(k).toFixed(3), true);
             inPerod = true;
+            if(k === prop.numKeys) animatedTimes.push([prop.keyTime(k).toFixed(3), -1]);
          }
       }
    }
    return animatedTimes;
 }
 
+function unionAnims(arr) {
+   var unionArr = [];
+   var stack = 0;
+   for(b = 0; b < arr.length; b++) {
+      if(stack === 0) unionArr.push(arr[b]);
+      stack += arr[b][1];
+      if(stack === 0) unionArr.push(arr[b]);
+   }
+   return unionArr;
+}
+
 function cutToKeys(layer) {
-   var allAmims = [];
+   var allAnims = [];
    for(var i = 1; i <= layer.numProperties; i++) {
       for(var j = 1; j <= layer.property(i).numProperties; j++) {
          var prop = layer.property(i).property(j);
          if(prop.numKeys) {
-            //getAnimatedSections()
-            alert(getAnimatedSections(prop));
+            var propAnims = getAnimatedSections(prop);
+            for(z = 0; z < propAnims.length; z++) {
+               allAnims.push(propAnims[z]);
+            }
          }
       }
    }
-   // layer.inPoint = firstKeyTime;
-   // layer.outPoint = lastKeyTime;
+   var unionSections = unionAnims(allAnims.sort(function(a, b) {
+      return a[0] - b[0];
+   }));
+
+   var count = 0;
+   for(i = 0; i < unionSections.length; i += 2) {
+      if(count) layer = layer.duplicate();
+      layer.inPoint = unionSections[i][0];
+      layer.outPoint = unionSections[i + 1][0];
+      count++;
+   }
 }
 
 var layers = [];
 
-// for (i = 0; i < std.selectedLayers.length; i++) {
-//    layers.push(std.selectedLayers[i]);
-// }
+for(i = 0; i < std.selectedLayers.length; i++) {
+   layers.push(std.selectedLayers[i]);
+}
 
-// for(var a = 0; a < layers.length; a++) {
-//    cutToKeys(layers[a]);
-// }
+app.beginUndoGroup("cut to keys");
+
+for(var a = 0; a < layers.length; a++) {
+   cutToKeys(layers[a]);
+}
+
+app.endUndoGroup();
